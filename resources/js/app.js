ea2d435ade6410f678d8a7ad65b11eab2a9b3d40
Justinedesch <justine.deschamps@le-campus-numerique.fr>
2023-07-05 11:59:31 +0200

    Mise en place templates/header/footer + AUTH (breeze)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,7 @@
 import './bootstrap';
-import 'flowbite';
-import {
-    Collapse,
-    Dropdown,
-    initTE,
-} from "tw-elements";
 
-initTE({ Collapse, Dropdown });
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
