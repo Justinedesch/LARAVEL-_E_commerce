62b9bba23e5b2e188578bfcebed05e3de0250da3
Sebastien73 <jssebastienjacquelin@hotmail.com>
2023-07-05 12:04:49 +0200

    fix tw prob

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,12 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+import 'flowbite';
+import {
+    Collapse,
+    Dropdown,
+    initTE,
+} from "tw-elements";
+
+initTE({ Collapse, Dropdown });
