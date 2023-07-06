a56c2daf94a3ac1591721b28869d17f76c217ac3
Justinedesch <justine.deschamps@le-campus-numerique.fr>
2023-07-06 14:02:24 +0200

    Ajout liens entre catalogue et produits + panier

--- resources/views/catalogue.blade.php
+++ resources/views/catalogue.blade.php
@@ -9,7 +9,7 @@ Catalogue
 @section('content')
 <div class="welcome">
     <h2 class="text-4xl font-extrabold dark:text-white">Nos Produits</h2>
-    <p class="my-4 text-lg text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    <p class="my-4 text-lg text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 </div>
         <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
@@ -19,8 +19,8 @@ Catalogue
                 <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
                 <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
             </div>
-            <div class="button-ca">    
-                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            <div class="button-ca">
+                <a href="{{url("/product/1")}}" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</a>
             </div>
         </div>
         <div>
@@ -29,8 +29,8 @@ Catalogue
                 <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
                 <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
             </div>
-            <div class="button-ca">    
-                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            <div class="button-ca">
+                <a href="{{url("/product/2")}}" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</a>
             </div>
         </div>
         <div>
@@ -39,8 +39,8 @@ Catalogue
                 <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
                 <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
             </div>
-            <div class="button-ca">    
-                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            <div class="button-ca">
+                <a href="{{url("/product/3")}}" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</a>
             </div>
         </div>
         <div>
@@ -49,8 +49,8 @@ Catalogue
                 <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
                 <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
             </div>
-            <div class="button-ca">    
-                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            <div class="button-ca">
+                <a href="{{url("/product/4")}}" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</a>
             </div>
         </div>
         <div>
@@ -59,8 +59,8 @@ Catalogue
                 <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
                 <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
             </div>
-            <div class="button-ca">    
-                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            <div class="button-ca">
+                <a href="{{url("/product/5")}}" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</a>
             </div>
         </div>
         <div>
@@ -69,8 +69,8 @@ Catalogue
                 <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
                 <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
             </div>
-            <div class="button-ca">    
-                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            <div class="button-ca">
+                <a href="{{url("/product/6")}}" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</a>
             </div>
         </div>
         <div>
@@ -79,8 +79,8 @@ Catalogue
                 <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
                 <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
             </div>
-            <div class="button-ca">    
-                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            <div class="button-ca">
+                <a href="{{url("/product/7")}}" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</a>
             </div>
         </div>
         <div>
@@ -89,8 +89,8 @@ Catalogue
                 <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
                 <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
             </div>
-            <div class="button-ca">    
-                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            <div class="button-ca">
+                <a href="{{url("/product/8")}}" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</a>
             </div>
         </div>
         <div>
@@ -99,8 +99,8 @@ Catalogue
                 <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
                 <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
             </div>
-            <div class="button-ca">    
-                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            <div class="button-ca">
+                <a href="{{url("/product/9")}}" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</a>
             </div>>
         </div>
         <div>
@@ -109,8 +109,8 @@ Catalogue
                 <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
                 <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
             </div>
-            <div class="button-ca">    
-                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            <div class="button-ca">
+                <a href="{{url("/product/10")}}" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</a>
             </div>
         </div>
         <div>
@@ -119,8 +119,8 @@ Catalogue
                 <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
                 <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
             </div>
-            <div class="button-ca">    
-                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            <div class="button-ca">
+                <a href="{{url("/product/11")}}" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</a>
             </div>
         </div>
         <div>
@@ -129,8 +129,8 @@ Catalogue
                 <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
                 <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
             </div>
-            <div class="button-ca">    
-                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            <div class="button-ca">
+                <a href="{{url("/product/12")}}" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</a>
             </div>
         </div>
     </div>
