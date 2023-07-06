ec5ab2e8dc47ffee3b517a788ed5f0929fb554ce
Maher DAKAKNI <maher.dakakni@Digital-Grenoble.local>
2023-07-06 13:31:03 +0200

    css page catalogue

--- resources/views/catalogue.blade.php
+++ resources/views/catalogue.blade.php
@@ -8,99 +8,136 @@ Catalogue
 
 @section('content')
 <div class="welcome">
-<h2 class="text-4xl font-extrabold dark:text-white">Nos Produits</h2>
-<p class="my-4 text-lg text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
+    <h2 class="text-4xl font-extrabold dark:text-white">Nos Produits</h2>
+    <p class="my-4 text-lg text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 </div>
+        <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="">
+            <div class="name-product">
+                <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
+                <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
+            </div>
+            <div class="button-ca">    
+                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            </div>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="">
+            <div class="name-product">
+                <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
+                <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
+            </div>
+            <div class="button-ca">    
+                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            </div>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="">
+            <div class="name-product">
+                <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
+                <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
+            </div>
+            <div class="button-ca">    
+                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            </div>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="">
+            <div class="name-product">
+                <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
+                <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
+            </div>
+            <div class="button-ca">    
+                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            </div>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="">
+            <div class="name-product">
+                <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
+                <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
+            </div>
+            <div class="button-ca">    
+                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            </div>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt="">
+            <div class="name-product">
+                <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
+                <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
+            </div>
+            <div class="button-ca">    
+                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            </div>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg" alt="">
+            <div class="name-product">
+                <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
+                <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
+            </div>
+            <div class="button-ca">    
+                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            </div>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg" alt="">
+            <div class="name-product">
+                <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
+                <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
+            </div>
+            <div class="button-ca">    
+                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            </div>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg" alt="">
+            <div class="name-product">
+                <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
+                <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
+            </div>
+            <div class="button-ca">    
+                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            </div>>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg" alt="">
+            <div class="name-product">
+                <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
+                <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
+            </div>
+            <div class="button-ca">    
+                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            </div>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg" alt="">
+            <div class="name-product">
+                <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
+                <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
+            </div>
+            <div class="button-ca">    
+                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            </div>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg" alt="">
+            <div class="name-product">
+                <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
+                <span class="text-5xl font-extrabold tracking-tight">49<span class="text-3xl font-semibold">€</span></span>
+            </div>
+            <div class="button-ca">    
+                <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
+            </div>
+        </div>
+    </div>
 
 
-<div class="grid grid-cols-2 md:grid-cols-3 gap-4">
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="">
-        <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
-        <span class="text-5xl font-extrabold tracking-tight">49</span>
-        <span class="text-3xl font-semibold">€</span>
-        <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="">
-        <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
-        <span class="text-5xl font-extrabold tracking-tight">49</span>
-        <span class="text-3xl font-semibold">€</span>
-        <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="">
-        <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
-        <span class="text-5xl font-extrabold tracking-tight">49</span>
-        <span class="text-3xl font-semibold">€</span>
-        <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="">
-        <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
-        <span class="text-5xl font-extrabold tracking-tight">49</span>
-        <span class="text-3xl font-semibold">€</span>
-        <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="">
-        <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
-        <span class="text-5xl font-extrabold tracking-tight">49</span>
-        <span class="text-3xl font-semibold">€</span>
-        <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt="">
-        <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
-        <span class="text-5xl font-extrabold tracking-tight">49</span>
-        <span class="text-3xl font-semibold">€</span>
-        <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg" alt="">
-        <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
-        <span class="text-5xl font-extrabold tracking-tight">49</span>
-        <span class="text-3xl font-semibold">€</span>
-        <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg" alt="">
-        <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
-        <span class="text-5xl font-extrabold tracking-tight">49</span>
-        <span class="text-3xl font-semibold">€</span>
-        <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg" alt="">
-        <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
-        <span class="text-5xl font-extrabold tracking-tight">49</span>
-        <span class="text-3xl font-semibold">€</span>
-        <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg" alt="">
-        <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
-        <span class="text-5xl font-extrabold tracking-tight">49</span>
-        <span class="text-3xl font-semibold">€</span>
-        <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg" alt="">
-        <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
-        <span class="text-5xl font-extrabold tracking-tight">49</span>
-        <span class="text-3xl font-semibold">€</span>
-        <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
-    </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg" alt="">
-        <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Product name</h3>
-        <span class="text-5xl font-extrabold tracking-tight">49</span>
-        <span class="text-3xl font-semibold">€</span>
-        <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Shop</button>
-    </div>
-</div>
+
+
 @endsection
 
 
