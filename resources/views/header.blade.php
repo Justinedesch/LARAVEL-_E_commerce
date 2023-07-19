ff0793bfecb30dfc830ea30be1d6c0b077a0f2c3
Justinedesch <justine.deschamps@le-campus-numerique.fr>
2023-07-19 15:38:23 +0200

    suppression timestamp product table

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -15,7 +15,7 @@
         <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
             <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white md:dark:bg-gray-900">
                 <li>
-                    <a href="/" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 md:dark:bg-transparent" aria-current="page">Accueil</a>
+                    <a href="/home" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 md:dark:bg-transparent" aria-current="page">Accueil</a>
                 </li>
 
                 <li>
